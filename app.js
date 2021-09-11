26e9b007a5da2e81d16774137216498c044d7529
Ryan Modrall <ryanemccall20@gmail.com>
2021-09-11 11:40:37 -0400

    Initial setup of repo

---
+++ app.js
@@ -0,0 +1,23 @@
+require ('dotenv').config()
+const express = require('express')
+const app = express()
+const {sequelize} = require('./db')
+
+;(async () => {
+    try{
+        await sequelize.authenticate();
+        console.log('Server is working')
+    } catch (error) {
+        console.log('Server is failing', error)
+    }
+    sequelize.sync({force: true})
+})()
+
+const port = 3000
+const auth = require('./controllers/Auth');
+
+app.use('/', auth)
+
+app.listen(port, () => {
+    console.log(`[Server]: App is listening on 3000.`)
+})
---
+++ db.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize(
+    process.env.DB_DBNAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres'
+    }
+)
+
+async function synceDb(sequelize, force = false){
+    try {
+        if (force)
+            await sequelize.sync({force: true})
+        else
+            await sequelize.sync()
+    } catch (err){
+        console.log(err)
+    }
+}
+
+module.exports = {
+    sequelize,
+    synceDb
+}
