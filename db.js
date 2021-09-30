cc3b43e38ed2e50b666879ac7d2af6a64f676609
Ryan Modrall <ryanemccall20@gmail.com>
2021-09-30 19:47:54 -0400

    Pushing to Heroku main

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ const sequelize = new Sequelize( process.env.DATABASE_URL,
     // process.env.DB_PASS,
     {
         //host: process.env.DB_HOST,
-        dialect: 'postgres'
+        dialect: 'postgres',
+        //ssl: process.env.ENVIRONMENT === 'production'
     }
 )
 
