866ce8849cba083ecd04d688bd2be29382f204aa
Ryan Modrall <ryanemccall20@gmail.com>
2021-09-30 20:09:59 -0400

    fixing enviroment

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize( process.env.DATABASE_URL,
     {
         //host: process.env.DB_HOST,
         dialect: 'postgres',
-        ssl: process.env.ENVIRONMENT === 'production'
+        ssl: true
     }
 )
 
