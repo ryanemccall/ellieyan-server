d45088b73df7c1baa76fa2bbbcc78fa8dcd26efb
Ryan Modrall <ryanemccall20@gmail.com>
2021-09-30 20:15:07 -0400

    fixing enviroment

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize( process.env.DATABASE_URL,
         dialectOptions: {
             ssl: { 
                 require: true,
-                rejectedUnauthorized: false
+                rejectUnauthorized: false
             }
         }
     }
