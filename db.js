d343b9c0f5b048436e7793e004690d7ca855209a
Ryan Modrall <ryanemccall20@gmail.com>
2021-10-05 13:21:21 -0400

    Added comment so I know how to get it run on local when needed

--- db.js
+++ db.js
@@ -14,6 +14,7 @@ const sequelize = new Sequelize( process.env.DATABASE_URL,
                 rejectUnauthorized: false
             }
         }
+        //have to comment out dialectOptions to run on local
     }
 )
 
